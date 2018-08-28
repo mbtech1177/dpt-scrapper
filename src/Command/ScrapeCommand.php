074a1a9f0df07c960033b752e8e19630f4d1f79a
zackad <zackad.dev@gmail.com>
2018-08-28 11:50:57 +0700

    console command to scrap data

---
+++ src/Command/ScrapeCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ScrapeCommand extends Command
+{
+    private $baseURI = 'https://infopemilu.kpu.go.id/pilkada2018/pemilih/dpt/1/BANTEN/';
+    private $suffix = 'listDps.json';
+    private $timestamp;
+    protected static $defaultName = 'app:scrape';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Scrap daftar pemilih tetap pilkada tahun 2018 di Provinsi Banten.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title('Memulai scrapping data ...');
+        $io->success('Proses scrapping telah selesai.');
+    }
+}
