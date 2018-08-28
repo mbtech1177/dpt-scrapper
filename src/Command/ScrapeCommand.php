8d8d48b8f42581383758a054c1a22406044f6724
zackad <zackad.dev@gmail.com>
2018-08-28 14:56:25 +0700

    [WIP]make request and create Pemilih entity from results

--- src/Command/ScrapeCommand.php
+++ src/Command/ScrapeCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Command;
 
+use App\Entity\Pemilih;
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -26,6 +28,7 @@ class ScrapeCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         $io->title('Memulai scrapping data ...');
+        $this->makeRequest();
         $io->success('Proses scrapping telah selesai.');
     }
 
@@ -33,5 +36,34 @@ class ScrapeCommand extends Command
     {
         $this->timestamp = '?_=' . (new \Datetime)->getTimestamp();
         return $path . $this->suffix . $this->timestamp;
+    }
+
+    private function makeRequest()
+    {
+        $client = new Client(['base_uri' => $this->baseURI]);
+        $response = $client->request('GET', $this->generatePath('KOTA SERANG/CIPOCOK JAYA/BANJAR AGUNG/1/'));
+        $this->savePemilih($response->getBody()->getContents());
+        return;
+    }
+
+    private function savePemilih(string $json)
+    {
+        $arrayPemilih = json_decode($json, true);
+        foreach ($arrayPemilih['aaData'] as $calonPemilih) {
+            if (!isset($calonPemilih['nik'])) {
+                print "Nothing to do.\n";
+                break;
+            }
+            $pemilih = new Pemilih;
+            $pemilih->setNama($calonPemilih['nama'])
+                ->setNik($calonPemilih['nik'])
+                ->setJenisKelamin($calonPemilih['jenisKelamin'])
+                // ->setAlamat()
+                // ->setKelurahan()
+                // ->setKecamatan()
+                // ->setKota()
+                // ->setProvinsi()
+            ;
+        }
     }
 }
